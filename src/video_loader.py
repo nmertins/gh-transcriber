5ff754a21bd114c9f4cf0ce932590cf5f66d55b5
Nathan Mertins <nmertins@ihmc.us>
2017-05-10 16:32:52 -0500

    added trimmed version of video with only gameplay

--- src/video_loader.py
+++ src/video_loader.py
@@ -1,16 +1,30 @@
 import cv2
+import sys
+
+DEFAULT_VIDEO_PATH = './../resources/gh-ps2-gameplay-trimmed.mp4'
 
 
-cap = cv2.VideoCapture('./../resources/gh-ps2-gameplay.mp4')
+def load_video(filepath: str) -> cv2.VideoCapture:
+    cap = cv2.VideoCapture()
+    cap.open(filepath)
+    return cap
 
-while cap.isOpened():
-    ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def main():
+    cap = load_video(DEFAULT_VIDEO_PATH)
+    while True:
+        ret, frame = cap.read()
 
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(33) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
